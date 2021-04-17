c56d07f88e9e724a293da33ee19a45c3767bc7ed
marideguia <82063754+marideguia@users.noreply.github.com>
2021-04-17 09:50:12 -0500

    Add files via upload

---
+++ HackUIOWA-Project/earthInvaders.py
@@ -0,0 +1,40 @@
+import pygame
+
+# Initialize the pygame
+pygame.init()
+
+# Create the screen: w, h
+screen = pygame.display.set_mode((800,600))
+
+# Title and Icon (32 pixels)
+icon = pygame.image.load('us.png') # ***Change game icon***
+pygame.display.set_icon(icon)
+pygame.display.set_caption("Earth Invaders")
+
+# Player
+playerImg = pygame.image.load('student.png')
+# Player coordinates by pixel: x - Left to right, y - Top to bottom
+playerX = 355
+playerY = 500
+
+# FN DEF: Displays player on the screen
+def player(x,y):
+    # Draws the player on the screen
+    screen.blit(playerImg, (x, y))
+
+# Game Loop
+running = True
+while running:
+    # Background Color: R, G, B
+    screen.fill((128, 128, 255))
+
+    # Exit game when player QUITS
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Display player on screen
+    player(playerX,playerY)
+
+    # Continously update display
+    pygame.display.update()
